178169cc80af7f72e14cc99d5439cbd7bea256ab
menggege <god_lm@yeah.net>
2018-10-12 09:18:34 +0800

    change

--- js/autopaly.js
+++ js/autopaly.js
@@ -1,19 +1,20 @@
 var flag = true;
 var tasks2 = [];
 function autoPlay(params) {
+    flag = true
     function _fn() {
             setTimeout(() => {
                 flag ? window.location.hash = 'step=1' : ''
             }, 500);
             setTimeout(() => {
                 flag ? window.location.hash = 'step=2' : ''
-            }, 2000);
+            }, 3000);
             setTimeout(() => {
                 flag ? window.location.hash = 'step=3' : ''
-            }, 4000);
+            }, 6000);
             setTimeout(() => {
                 flag ? navigation(map, _fn) : ''
-            }, 6000);
+            }, 9000);
     }
     _fn()
 }
@@ -30,6 +31,6 @@ $('.header .title').click(function () {
 })
 $('.header .title').dblclick(function () {
 
-    flag = true
+
     autoPlay()
 })
